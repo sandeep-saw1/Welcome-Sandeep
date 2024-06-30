5ccd5d9e1b4b691f84ed37e6bc685d7f3174c24f
sandeep-saw1 <sawsandeep74@gmail.com>
2024-06-30 15:19:57 +0530

    Add files via upload

---
+++ myfirstcode.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a;
+
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+
+    cout<<"sum the no of a and b"<<a+b;
+
+    return 0;
+}
